e5a9dda82fc49807cf420e275a4e640a62f0d33b
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-02-21 03:11:13 +0200

    refactor the code

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="background bg-slate-50 h-[100dvh]">
+      <div className="max-w-7xl mx-auto">
+        <p className="py-2">Chat Websockets</p>
+      </div>
+    </div>
+  )
+}
--- web/src/App.tsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <div className="text-lg">Chat Websockets</div>
-}
