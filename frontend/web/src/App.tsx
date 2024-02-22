b0f5a78e2325bdd483adcc0532cf783b5557d291
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-02-22 04:57:38 +0200

    configurations

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,4 +1,12 @@
 export default function App() {
+  const chatSocket = new WebSocket("ws://" + window.location.host + "/ws/chat/")
+
+  chatSocket.onmessage = function () {}
+
+  chatSocket.onclose = function () {
+    console.error("Chat socket closed unexpectedly")
+  }
+
   return (
     <div className="background bg-slate-50 h-[100dvh]">
       <div className="max-w-7xl mx-auto">
